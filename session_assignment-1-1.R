9dfd282e7702a2eed97cfeb05c3d2c558968ace1
gopikrishnareddyR <40296528+gopikrishnareddyR@users.noreply.github.com>
2018-07-30 11:16:16 +0530

    Add files via upload

---
+++ session_assignment-1-1.R
@@ -0,0 +1,20 @@
+#Session1 _Assignmnet-1-1
+
+#1.Say true or false for the below statements:
+
+#prescriptive Analytics Used to predict the future outcomes? yes
+
+# yes
+#Base R packages Installed automatically? yes
+
+# yes
+
+# 2. What is recycling of elements in a vector?
+# when applying an operation to 2 vectors that requires them to be the same length, R automatically recycles,or 
+#repeates elements of shorter one, until it is log enough to match the longer vector.
+
+#3. give an Example of recycling of elements.
+
+v<- c(1,2,3)
+t<-c(1,2,3,4,5,6)
+v+t
